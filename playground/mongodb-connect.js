0f43ab77d4288885a9fe1905ce24c9967d2a3eba
Pedro Teixeira <pedro.ap.teixeira@gmail.com>
2016-11-25 16:29:34 +0000

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) return console.log('Unable to connect to MongoDB server');
+
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('58384e1575ab5c26168b2e10')
+  // }).toArray().then(docs => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, null, 2));
+  // }, err => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then(count => {
+  //   console.log(`Todos count: ${count}`);
+  // }, err => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({
+    name: 'Derp'
+  }).toArray().then(docs => {
+    console.log(JSON.stringify(docs, null, 2));
+  }, err => {
+    console.log('Error', err);
+  });
+
+  db.close();
+});
