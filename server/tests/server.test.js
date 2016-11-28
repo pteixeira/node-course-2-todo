7ed632f1f3418459993eac35eafa3ef28c603885
Pedro Teixeira <pedro.ap.teixeira@gmail.com>
2016-11-28 19:37:34 +0000

    Something

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -104,4 +104,43 @@ describe('GET /todos/:id', () => {
       .expect(404)
       .end(done);
   });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', done => {
+    var hexId = todos[0]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+
+        // query database using findbyid
+        // expect(todoArg).toNotExist()
+        Todo.findById(hexId).then(doc => {
+          expect(doc).toNotExist();
+          done();
+        }).catch(e => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', done => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if objectid is invalid', done => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
 })
